90ee00190776cffa8cd235de8104d89aec61088c
DaniilPoklonskiy <mr.sharko2015@mail.ru>
2023-10-29 22:01:57 +0300

    First commit

--- src/ru/mirea/lab14/RegexTest.java
+++ src/ru/mirea/lab14/RegexTest.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 public class RegexTest {
 public static void main(String[] args) {
 String input = "abcdefghijklmnopqrstuv18340";
-String regex = "^[a-z0-9]+$";
+String regex = "^[a-z0-9]+$";git
 
 boolean isMatch = Pattern.matches(regex, input);
 System.out.println(isMatch); // true
